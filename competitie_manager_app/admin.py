9be26d5db9cd1ce29b375641d8b5630f9fa7e848
joost-lauwen <lauwenjoost@gmail.com>
2020-08-12 21:34:23 +0200

    Added match model

--- competitie_manager_app/admin.py
+++ competitie_manager_app/admin.py
@@ -3,9 +3,12 @@ from competitie_manager_app.models.sport import Sport
 from competitie_manager_app.models.competition import Competition
 from competitie_manager_app.models.team import Team
 from competitie_manager_app.models.team_competition import TeamCompetition
+from competitie_manager_app.models.match import Match
+
 
 # Register your models here.
 admin.site.register(Sport)
 admin.site.register(Competition)
 admin.site.register(Team)
 admin.site.register(TeamCompetition)
+admin.site.register(Match)
--- competitie_manager_app/models/__init__.py
+++ competitie_manager_app/models/__init__.py
@@ -3,3 +3,4 @@ from .team import *
 from .sport import *
 from .competition import *
 from .team_competition import *
+from .match import *
