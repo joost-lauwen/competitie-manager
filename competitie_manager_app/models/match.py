35eb77636299f20d9c1b0e4fb3a810ab7da55567
joost-lauwen <lauwenjoost@gmail.com>
2020-08-13 14:48:50 +0200

    Matches are automatically generated for the competition

--- competitie_manager_app/models/match.py
+++ competitie_manager_app/models/match.py
@@ -1,17 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.urls import reverse
 from .competition import Competition
+from .team import Team
 
 
 class Match(models.Model):
     home_team = models.ForeignKey(
-        User, null=True, on_delete=models.SET_NULL, related_name='match_home_team')
+        Team, null=True, on_delete=models.SET_NULL, related_name='match_home_team')
     away_team = models.ForeignKey(
-        User, null=True, on_delete=models.SET_NULL, related_name='match_away_team')
+        Team, null=True, on_delete=models.SET_NULL, related_name='match_away_team')
     competition = models.ForeignKey(
         Competition, null=True, on_delete=models.SET_NULL, related_name='competition_match')
     result = models.IntegerField(null=True, default=0)
 
     def __str__(self):
-        return self.competition.naam + ' // ' + self.home_team + ' vs ' + self.away_team
+        return self.competition.naam + ' // ' + self.home_team.naam + ' vs ' + self.away_team.naam
--- competitie_manager_app/views/create_competition_view.py
+++ competitie_manager_app/views/create_competition_view.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from competitie_manager_app.models.competition import Competition
 from competitie_manager_app.models.team import Team
 from competitie_manager_app.models.team_competition import TeamCompetition
+from competitie_manager_app.models.match import Match
 from competitie_manager_app.forms.competition_form import CompetitionCreateForm
 
 
@@ -26,4 +27,47 @@ class CreateCompetitionView(LoginRequiredMixin, CreateView):
         for team in comp_teams:
             team.competition.add(form.instance)
 
+        round_robin_teams = list(comp_teams)
+
+        rounds = (len(round_robin_teams) - 1) * 2
+        matches = CreateCompetitionView.round_robin(round_robin_teams, rounds)
+
+        for round in matches:
+
+            for match in round:
+                home_team = match[0]
+                away_team = match[1]
+
+                if home_team is None:
+                    pass
+                elif away_team is None:
+                    pass
+                else:
+                    Match.objects.create(
+                        home_team=home_team, away_team=away_team, competition=form.instance)
+
         return redirect_url
+
+    def round_robin(teams, rounds):
+
+        if len(teams) % 2:
+            teams.append(None)
+
+        schedule = []
+
+        for turn in range(rounds):
+            pairings = []
+
+            for i in range(len(teams) // 2):
+                if turn % 2:
+                    home = teams[i]
+                    away = teams[len(teams) - i - 1]
+                else:
+                    home = teams[len(teams) - i - 1]
+                    away = teams[i]
+                pairings.append((home, away))
+
+            teams.insert(1, teams.pop())
+            schedule.append(pairings)
+
+        return schedule
